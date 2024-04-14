6379c8abb979e0af3a99668eb4c2743640c76f91
KagisoMakgaba1 <kagisomakgaba99@gmail.com>
2024-04-14 10:21:55 -0700

    setup_mysql_dev.sql

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,14 +1,14 @@
--- This script is used to setup the MySQL database for the
--- development environment
+-- This script is used to setup the MySQL database for the 
+-- development environment.
 
 -- Create the database
 CREATE DATABASE IF NOT EXISTS `hbnb_dev_db`;
 
 -- Create the user
-CREATE USER IF NOT EXISTS `hbnb_dev` @`localhost` IDENTIFIED BY `hbnb_dev_pwd`;
+CREATE USER IF NOT EXISTS 'hbnb_dev' @'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
--- Create the user privileges
-GRANT USAGE ON *.* TO `hbnb_dev` @`localhost`;
+-- Grant the user privileges
+GRANT USAGE ON *.* TO 'hbnb_dev' @'localhost';
 GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev' @'localhost';
 GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev' @'localhost';
 
