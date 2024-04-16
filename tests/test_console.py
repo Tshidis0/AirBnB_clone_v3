7cdfd8519eb7c28ece34e53ed658171c7e76cbe8
KagisoMakgaba1 <kagisomakgaba99@gmail.com>
2024-04-16 12:08:33 -0700

    test_console.py

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,122 +1,131 @@
 #!/usr/bin/python3
+"""Unittest module for the console"""
 
-"""This module tests the console."""
-
-import os
 import unittest
-from unittest.mock import patch
-from io import StringIO
-import sqlalchemy
+import os
+import json
+import pycodestyle
+import io
 from console import HBNBCommand
-from lazy_methods import LazyMethods
-import models
+from models.engine.file_storage import FileStorage
+from unittest.mock import patch
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
-instance = LazyMethods()
 
 
-class TestConsole(unittest.TestCase):
-    """Tests the console."""
-
+class TestCommand(unittest.TestCase):
+    """Class that tests the console"""
+    
     def setUp(self):
-        self.cli = HBNBCommand()
+        """Function empties file.json"""
+        FileStorage._FileStorage__objects = {}
+        FileStorage().save()
 
-        if os.getenv("HBNB_TYPE_STORAGE") == "db":
-            models.storage.rollback()
+    @unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') == 'db', "Not FileStorage")
+    def test_create_fs(self):
+        """test the create command"""
+        storage = FileStorage()
+        storage.reload()
+        opt = r'[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[0-9a-f]{4}-[0-9a-f]{12}'
+        with self.assertRaises(AttributeError):
+            with patch('sys.stdout', new=io.StringIO()) as f:
+                HBNBCommand().onecmd("create BaseModel updated_at=0.0"
+                                     " created_at=0.0")
+        with patch('sys.stdout', new=io.StringIO()) as f:
+            HBNBCommand().onecmd('create User email="cluck@wanadoo.fr"'
+                                 ' password="jesustakethewheel"')
+        result = f.getvalue().strip()
+        self.assertRegex(result, opt)
+        email = storage.all()[f'User.{result}'].email
+        self.assertEqual(email, "cluck@wanadoo.fr")
+        password = storage.all()[f'User.{result}'].password
+        self.assertEqual(password, "jesustakethewheel")
+        with patch('sys.stdout', new=io.StringIO()) as f:
+            HBNBCommand().onecmd('create State johnny="bravo"'
+                                 ' number="7" pi="3.14"')
+        result = f.getvalue().strip()
+        self.assertRegex(result, opt)
+        johnny = storage.all()[f'State.{result}'].johnny
+        self.assertEqual(johnny, "bravo")
+        number = storage.all()[f'State.{result}'].number
+        self.assertEqual(number, '7')
+        pi = storage.all()[f'State.{result}'].pi
+        self.assertEqual(pi, '3.14')
+        with patch('sys.stdout', new=io.StringIO()) as f:
+            HBNBCommand().onecmd('create City johnny="bravo" number="7"'
+                                 ' pi="3.14"')
+        result = f.getvalue().strip()
+        self.assertRegex(result, opt)
+        johnny = storage.all()[f'City.{result}'].johnny
+        self.assertEqual(johnny, "bravo")
+        number = storage.all()[f'City.{result}'].number
+        self.assertEqual(number, '7')
+        pi = storage.all()[f'City.{result}'].pi
+        self.assertEqual(pi, '3.14')
+        with patch('sys.stdout', new=io.StringIO()) as f:
+            HBNBCommand().onecmd('create Amenity johnny="bravo"'
+                                 ' number="7" pi="3.14"')
+        result = f.getvalue().strip()
+        self.assertRegex(result, opt)
+        johnny = storage.all()[f'Amenity.{result}'].johnny
+        self.assertEqual(johnny, "bravo")
+        number = storage.all()[f'Amenity.{result}'].number
+        self.assertEqual(number, '7')
+        pi = storage.all()[f'Amenity.{result}'].pi
+        self.assertEqual(pi, '3.14')
+        with patch('sys.stdout', new=io.StringIO()) as f:
+            HBNBCommand().onecmd('create Place johnny="bravo"'
+                                 ' number="7" pi="3.14"')
+        result = f.getvalue().strip()
+        self.assertRegex(result, opt)
+        johnny = storage.all()[f'Place.{result}'].johnny
+        self.assertEqual(johnny, "bravo")
+        number = storage.all()[f'Place.{result}'].number
+        self.assertEqual(number, '7')
+        pi = storage.all()[f'Place.{result}'].pi
+        self.assertEqual(pi, '3.14')
+        with patch('sys.stdout', new=io.StringIO()) as f:
+            HBNBCommand().onecmd('create Review johnny="bravo"'
+                                 ' number="7" pi="3.14"')
+        result = f.getvalue().strip()
+        self.assertRegex(result, opt)
+        johnny = storage.all()[f'Review.{result}'].johnny
+        self.assertEqual(johnny, "bravo")
+        number = storage.all()[f'Review.{result}'].number
+        self.assertEqual(number, '7')
+        pi = storage.all()[f'Review.{result}'].pi
+        self.assertEqual(pi, '3.14')
+        with patch('sys.stdout', new=io.StringIO()) as f:
+            HBNBCommand().onecmd('create')
+        opt = '** class name missing **\n'
+        self.assertEqual(f.getvalue(), opt)
+        with patch('sys.stdout', new=io.StringIO()) as f:
+            HBNBCommand().onecmd('create NotClass')
+        opt = '** class doesn\'t exist **\n'
+        self.assertEqual(f.getvalue(), opt)
 
-    def test_do_create_no_args(self):
-        """Test `create` with no model name."""
-        with patch("sys.stdout", new=StringIO()) as f:
-            self.cli.onecmd("create")
-            self.assertEqual(f.getvalue(), "** class name missing **\n")
+    def testPycodeStyle(self):
+        """Pycodestyle test for console.py"""
+        style = pycodestyle.StyleGuide(quiet=True)
+        p = style.check_files(['console.py'])
+        self.assertEqual(p.total_errors, 0, "fix pep8")
 
-    def test_do_create_invalid_class(self):
-        """Test `create` with an invalid model name."""
-        with patch("sys.stdout", new=StringIO()) as f:
-            self.cli.onecmd("create MyClass")
-            self.assertEqual(f.getvalue(), "** class doesn't exist **\n")
-
-    @unittest.skipIf(os.getenv("HBNB_TYPE_STORAGE") == "db", "file")
-    def test_do_create_valid_class(self):
-        """Test `create` with a valid model name."""
-        with patch("sys.stdout", new=StringIO()) as f:
-            self.cli.onecmd("create BaseModel")
-            self.assertNotEqual(f.getvalue(), "** class doesn't exist **\n")
-            self.assertNotEqual(f.getvalue(), "** class name missing **\n")
-
-    @unittest.skipIf(os.getenv("HBNB_TYPE_STORAGE") == "db", "file")
-    def test_do_create_one_attribute(self):
-        """Test `create` with one attribute."""
-        with patch("sys.stdout", new=StringIO()) as f:
-            self.cli.onecmd('create BaseModel name="Holberton"')
-
-            key = instance.get_key("BaseModel", instance.get_uuid(f))
-            obj = models.storage.all()[key]
-            self.assertTrue(
-                set({"name": "Holberton"}.items()).issubset(
-                    obj.__dict__.items()
-                )
-            )
-
-    def test_do_create_escaped_str(self):
-        """Test `create` with an escaped string attribute value."""
-        with patch("sys.stdout", new=StringIO()) as f:
-            self.cli.onecmd(
-                'create User first_name=""Betty"_Holberton" '
-                'email="bettyholberton@hbtn.io" password="abc123"'
-            )
-
-            key = instance.get_key("User", instance.get_uuid(f))
-            obj = models.storage.all()[key]
-            self.assertTrue(
-                set(
-                    {
-                        "first_name": '"Betty" Holberton',
-                        "email": "bettyholberton@hbtn.io",
-                        "password": "abc123",
-                    }.items()
-                ).issubset(obj.__dict__.items()),
-            )
-
-    def test_do_create_multiple_attributes(self):
-        """Test `create` with multiple attributes."""
-        with patch("sys.stdout", new=StringIO()) as f:
-            self.cli.onecmd(
-                'create User first_name="Betty" last_name="Holberton" '
-                'email="bettyholberton@hbtn.io" password="abc123"'
-            )
-
-            key = instance.get_key("User", instance.get_uuid(f))
-            obj = models.storage.all()[key]
-            self.assertTrue(
-                set(
-                    {
-                        "first_name": "Betty",
-                        "last_name": "Holberton",
-                        "email": "bettyholberton@hbtn.io",
-                        "password": "abc123",
-                    }.items()
-                ).issubset(obj.__dict__.items()),
-            )
-
-    def test_do_create_invalid_attribute(self):
-        """Test `create` with an invalid attribute."""
-        if os.getenv("HBNB_TYPE_STORAGE") == "db":
-            print("Using DB")
-            with self.assertRaises(sqlalchemy.exc.IntegrityError):
-                self.cli.onecmd(
-                    'create User invalid_attribute="Betty Holberton"'
-                )
-        else:
-            print("Using File storage")
-            with patch("sys.stdout", new=StringIO()) as f:
-                self.cli.onecmd(
-                    'create User invalid_attribute="Betty Holberton"'
-                )
-
-                key = instance.get_key("User", instance.get_uuid(f))
-                obj = models.storage.all()[key]
-                self.assertNotIn("invalid_attribute", obj.__dict__)
+    def test_doc_console(self):
+        self.assertIsNotNone(HBNBCommand.__doc__)
+        self.assertIsNotNone(HBNBCommand.do_all.__doc__)
+        self.assertIsNotNone(HBNBCommand.do_create.__doc__)
+        self.assertIsNotNone(HBNBCommand.do_destroy.__doc__)
+        self.assertIsNotNone(HBNBCommand.do_quit.__doc__)
+        self.assertIsNotNone(HBNBCommand.do_EOF.__doc__)
+        self.assertIsNotNone(HBNBCommand.do_count.__doc__)
+        self.assertIsNotNone(HBNBCommand.do_update.__doc__)
+        self.assertIsNotNone(HBNBCommand.emptyline.__doc__)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
